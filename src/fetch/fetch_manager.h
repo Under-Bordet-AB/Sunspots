7def05c3c96e9baf3d1b85b69550964e4da4034f
Gustav Frisén <gustav.frisen@outlook.com>
2026-01-27 13:35:47 +0100

    Add forking

--- src/fetch/fetch_manager.h
+++ src/fetch/fetch_manager.h
@@ -1,30 +1,57 @@
 #ifndef FETCH_MANAGER_H
 #define FETCH_MANAGER_H
 
+#include <signal.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 // #include "curl_client.h"
 // #include "parser_smhi.h"
 // #include "parser_elpris.h"
 // #include "database_manager.h"
 
-typedef struct fetch_manager {
-} fetch_manager;
+static pid_t fetch_pid = -1;
+
+void fetch_cleanup() {
+    printf("Cleaning up fetch manager.\n");
+
+    if (fetch_pid > 0) {
+        kill(fetch_pid, SIGTERM);
+        waitpid(fetch_pid, NULL, 0);
+    }
+}
 
 void fetch_start() {
     printf("Starting fetch manager.\n");
 
-    while (1) {
-        // once every 15 minutes
-        // start fetch threads from the functions below, poll them, save to database when done
-        // signal to parent that the fetching is done
+    int iterations = 0;
+    int max_iterations = 5;
+
+    while (iterations < max_iterations) {
+        printf("RUNNING!!! Iteration %d/%d\n", iterations + 1, max_iterations);
+
+        iterations++;
+
+        if (iterations < max_iterations) {
+            sleep(1);
+        }
     }
 }
 
 void fetch_init() {
     printf("Initializing fetch manager.\n");
 
-    fetch_start();
+    fetch_pid = fork();
+
+    if (fetch_pid < 0) {
+        perror("Fork failed");
+        return;
+    } else if (fetch_pid == 0) {
+        fetch_start();
+        _exit(0);
+    }
 }
 
 int fetch_from_smhi() {
--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
+#include <stdlib.h>
+#include <sys/wait.h>
+
 #include "fetch/fetch_manager.h"
 
 int main() {
     fetch_init();
+    atexit(fetch_cleanup);
+
+    if (fetch_pid > 0) {
+        int status;
+        waitpid(fetch_pid, &status, 0);
+    }
 
     return 0;
 }
