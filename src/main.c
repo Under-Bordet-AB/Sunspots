500808b8c5ce46b245c75f15dbfe4b435990ccd7
Gustav Frisén <gustav.frisen@outlook.com>
2026-01-28 23:24:26 +0100

    Rename module to curly

--- src/main.c
+++ src/main.c
@@ -2,35 +2,35 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#include "fetch/curl_client.h"
+#include "fetch/curly.h"
 
 #define url_mock "http://example.com"
 #define url_httpbin "http://httpbin.org/get"
 
 int main() {
-    printf("Curl on that thang\n");
+    printf("Curly starting...\n");
 
-    curl_client_t* curl_client;
-    curl_client_init(&curl_client);
+    curly_t* curl_client;
+    curly_init(&curl_client);
 
-    curl_client_make_request(&curl_client, url_httpbin);
+    curly_make_request(&curl_client, url_httpbin);
 
-    while (curl_client_poll(&curl_client) == 0) {
+    while (curly_poll(&curl_client) == 0) {
         printf("curling\n");
 
-        if (curl_client_is_running(&curl_client) == 0) {
+        if (curly_is_running(&curl_client) == 0) {
             break;
         }
     }
 
     char* buffer = NULL;
-    curl_client_read_response(&curl_client, &buffer);
+    curly_read_response(&curl_client, &buffer);
 
     printf("%s\n", buffer);
 
     free(buffer);
 
-    curl_client_cleanup(&curl_client);
+    curly_cleanup(&curl_client);
 
     return 0;
 }
