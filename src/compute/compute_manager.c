3bd7e9efaf2ae500fa4fe5e2ec05e45789e4183c
Gustav Frisén <gustav.frisen@outlook.com>
2026-02-02 14:15:55 +0100

    Add heartbeat

--- src/compute/compute_manager.c
+++ src/compute/compute_manager.c
@@ -1,8 +1,17 @@
+#include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+pid_t g_ppid = 0;
+
+void* heartbeat();
 
 int main(int argc, char* argv[]) {
-    if (argc < 3) {
+    if (argc < 1) {
         fprintf(stderr, "Usage: ...\n");
         return EXIT_FAILURE;
     }
@@ -10,12 +19,32 @@ int main(int argc, char* argv[]) {
     printf("Compute manager started\n");
 
     // Parse arguments
-    // char* endptr;
-    // g_ppid = (int)strtol(argv[1], &endptr, 10);
-    // if (*endptr != '\0') return EXIT_FAILURE;
+    char* endptr;
+    g_ppid = (int) strtol(argv[1], &endptr, 10);
+    if (*endptr != '\0')
+        return EXIT_FAILURE;
 
-    // g_interval = (int)strtol(argv[2], &endptr, 10);
-    // if (*endptr != '\0') return EXIT_FAILURE;
+    pthread_t thread_hb;
+    pthread_create(&thread_hb, NULL, heartbeat, NULL);
+    pthread_detach(thread_hb);
+
+    while (1) {
+        // EPOLL (database file)
+        sleep(5);
+    }
 
     return 0;
+}
+
+void* heartbeat() {
+    while (1) {
+        if (kill(g_ppid, SIGRTMIN) == -1) {
+            perror("Could not signal daemon, terminating.\n");
+            exit(EXIT_FAILURE);
+        }
+        printf("Beating...\n");
+        sleep(1);
+    }
+
+    return NULL;
 }
