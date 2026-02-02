5d788d5c581ba44febc0e5bb1a3a3910a2fefe99
Gustav Frisén <gustav.frisen@outlook.com>
2026-02-02 16:20:15 +0100

    Add inotify to only work when a certain file has been modified

--- src/compute/compute_manager.c
+++ src/compute/compute_manager.c
@@ -1,14 +1,17 @@
-#include <pthread.h>
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/inotify.h>
 #include <unistd.h>
+#include <string.h>
 
 pid_t g_ppid = 0;
 
 void* heartbeat();
+int compute_work();
 
 int main(int argc, char* argv[]) {
     if (argc < 1) {
@@ -24,24 +27,50 @@ int main(int argc, char* argv[]) {
     if (*endptr != '\0')
         return EXIT_FAILURE;
 
+    // Initialize inotify
+    int inotify_fd = inotify_init();
+    if (inotify_fd < 0) {
+        perror("inotify_init failed");
+        return EXIT_FAILURE;
+    }
+
+    // Watch file for modifications
+    const char* db_path = "tester";
+    int watch_fd = inotify_add_watch(inotify_fd, db_path, IN_CLOSE_WRITE);
+    if (watch_fd < 0) {
+        perror("inotify_add_watch failed");
+        close(inotify_fd);
+        return EXIT_FAILURE;
+    }
+
+    // Heartbeat
     pthread_t thread_hb;
     pthread_create(&thread_hb, NULL, heartbeat, NULL);
     pthread_detach(thread_hb);
 
+    char buffer[4096];
     while (1) {
-        // EPOLL (database file)
-        sleep(5);
+        int len = read(inotify_fd, buffer, sizeof(buffer));
+        if (len > 0) {
+            compute_work();
+        }
     }
+
+    return 0;
+}
+
+int compute_work() {
+    printf("Compute working\n");
 
     return 0;
 }
 
 void* heartbeat() {
     while (1) {
-        if (kill(g_ppid, SIGRTMIN) == -1) {
-            perror("Could not signal daemon, terminating.\n");
-            exit(EXIT_FAILURE);
-        }
+        // if (kill(g_ppid, SIGRTMIN) == -1) {
+        //     perror("Could not signal daemon, terminating.\n");
+        //     exit(EXIT_FAILURE);
+        // }
         printf("Beating...\n");
         sleep(1);
     }
