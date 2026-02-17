53ac66a7b77238a112b46ed573e64b2c62ee1688
jonakack <jonathan.kacker@gmail.com>
2026-02-17 12:21:33 +0100

    Test changes

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -4,9 +4,11 @@
 
 int main()
 {
+    Menu menu;
+    menu.main();
+
     HttpClient httpClient(HOST, PORT);
-    
-    std::string response = httpClient.get("/test.txt");
+    std::string response = httpClient.get("/option1.txt");
     std::cout << response << "\n";
     
     return 0;
