2d3260fe32fb35e79035a36d52b2b1b70f26ba83
Jimmy Jordan <169525180+jimmyjordanSWE@users.noreply.github.com>
2026-02-20 10:29:21 +0000

    build system test script updated

--- scripts/test_make_cli_matrix.sh
+++ scripts/test_make_cli_matrix.sh
@@ -9,6 +9,8 @@ RUN_TIMEOUT_SECONDS="${RUN_TIMEOUT_SECONDS:-8}"
 VALGRIND_RUN_SECONDS="${VALGRIND_RUN_SECONDS:-3}"
 INCLUDE_HEAVY="${INCLUDE_HEAVY:-1}"
 INCLUDE_RUN_TARGETS="${INCLUDE_RUN_TARGETS:-1}"
+ALLOW_TEST_FAILURES="${ALLOW_TEST_FAILURES:-1}"
+ALLOW_RUNTIME_FAILURES="${ALLOW_RUNTIME_FAILURES:-1}"
 
 declare -a MAKE_BASE
 MAKE_BASE=("$MAKE_BIN" -C "$ROOT_DIR" --no-print-directory)
@@ -82,8 +84,8 @@ fi
 
 printf "Sunspots Make CLI matrix test\n"
 printf "Repo: %s\n" "$ROOT_DIR"
-printf "RUN_TIMEOUT_SECONDS=%s VALGRIND_RUN_SECONDS=%s INCLUDE_HEAVY=%s INCLUDE_RUN_TARGETS=%s\n" \
-  "$RUN_TIMEOUT_SECONDS" "$VALGRIND_RUN_SECONDS" "$INCLUDE_HEAVY" "$INCLUDE_RUN_TARGETS"
+printf "RUN_TIMEOUT_SECONDS=%s VALGRIND_RUN_SECONDS=%s INCLUDE_HEAVY=%s INCLUDE_RUN_TARGETS=%s ALLOW_TEST_FAILURES=%s ALLOW_RUNTIME_FAILURES=%s\n" \
+  "$RUN_TIMEOUT_SECONDS" "$VALGRIND_RUN_SECONDS" "$INCLUDE_HEAVY" "$INCLUDE_RUN_TARGETS" "$ALLOW_TEST_FAILURES" "$ALLOW_RUNTIME_FAILURES"
 
 run_case "deepclean (initial)" "0" "${MAKE_BASE[@]}" deepclean
 run_case "help" "0" "${MAKE_BASE[@]}" help
@@ -97,15 +99,33 @@ if [ "${#all_modules[@]}" -eq 0 ]; then
 fi
 
 mapfile -t test_modules < <(printf '%s\n' "${all_modules[@]}" | awk '/_test$/')
+mapfile -t runtime_modules < <(printf '%s\n' "${all_modules[@]}" | awk '!/_test$/')
 
 all_modules_csv="$(to_csv "${all_modules[@]}")"
 test_modules_csv="$(to_csv "${test_modules[@]}")"
+runtime_modules_csv="$(to_csv "${runtime_modules[@]}")"
+
+test_expected_rc="0"
+all_expected_rc="0"
+if [ "$ALLOW_TEST_FAILURES" = "1" ]; then
+  test_expected_rc="0,2"
+  all_expected_rc="0,2"
+fi
+
+run_expected_rc="0,124"
+if [ "$ALLOW_RUNTIME_FAILURES" = "1" ]; then
+  run_expected_rc="0,2,124"
+fi
 
 printf "\nDiscovered %s module targets\n" "${#all_modules[@]}"
 printf "  %s\n" "${all_modules[@]}"
 printf "\nDiscovered %s test module targets\n" "${#test_modules[@]}"
 if [ "${#test_modules[@]}" -gt 0 ]; then
   printf "  %s\n" "${test_modules[@]}"
+fi
+printf "\nDiscovered %s runtime module targets\n" "${#runtime_modules[@]}"
+if [ "${#runtime_modules[@]}" -gt 0 ]; then
+  printf "  %s\n" "${runtime_modules[@]}"
 fi
 
 run_case "build (all)" "0" "${MAKE_BASE[@]}" build
@@ -125,31 +145,31 @@ if [ "${#test_modules[@]}" -gt 0 ]; then
   for test_module in "${test_modules[@]}"; do
     run_case "build-tests M=$test_module" "0" "${MAKE_BASE[@]}" build-tests "M=$test_module"
     run_case "build-tests-valgrind M=$test_module" "0" "${MAKE_BASE[@]}" build-tests-valgrind "M=$test_module"
-    run_case "run-tests M=$test_module" "0" "${MAKE_BASE[@]}" run-tests "M=$test_module"
-    run_case "run-tests-valgrind M=$test_module" "0" "${MAKE_BASE[@]}" run-tests-valgrind "M=$test_module"
+    run_case "run-tests M=$test_module" "$test_expected_rc" "${MAKE_BASE[@]}" run-tests "M=$test_module"
+    run_case "run-tests-valgrind M=$test_module" "$test_expected_rc" "${MAKE_BASE[@]}" run-tests-valgrind "M=$test_module"
   done
 
   run_case "build-tests M=<all-test-modules-combined>" "0" "${MAKE_BASE[@]}" build-tests "M=$test_modules_csv"
   run_case "build-tests-valgrind M=<all-test-modules-combined>" "0" "${MAKE_BASE[@]}" build-tests-valgrind "M=$test_modules_csv"
-  run_case "run-tests M=<all-test-modules-combined>" "0" "${MAKE_BASE[@]}" run-tests "M=$test_modules_csv"
-  run_case "run-tests-valgrind M=<all-test-modules-combined>" "0" "${MAKE_BASE[@]}" run-tests-valgrind "M=$test_modules_csv"
+  run_case "run-tests M=<all-test-modules-combined>" "$test_expected_rc" "${MAKE_BASE[@]}" run-tests "M=$test_modules_csv"
+  run_case "run-tests-valgrind M=<all-test-modules-combined>" "$test_expected_rc" "${MAKE_BASE[@]}" run-tests-valgrind "M=$test_modules_csv"
 fi
 
 if [ "$INCLUDE_RUN_TARGETS" = "1" ]; then
   if [ -n "$timeout_cmd" ]; then
-    for module in "${all_modules[@]}"; do
-      run_case "run M=$module (timeout)" "0,124" \
+    for module in "${runtime_modules[@]}"; do
+      run_case "run M=$module (timeout)" "$run_expected_rc" \
         "$timeout_cmd" "${RUN_TIMEOUT_SECONDS}s" "${MAKE_BASE[@]}" run "M=$module"
-      run_case "run-valgrind M=$module (timeout)" "0,124" \
+      run_case "run-valgrind M=$module (timeout)" "$run_expected_rc" \
         "$timeout_cmd" "$((VALGRIND_RUN_SECONDS + 8))s" \
         "${MAKE_BASE[@]}" run-valgrind "M=$module" "VALGRIND_RUN_SECONDS=$VALGRIND_RUN_SECONDS"
     done
 
     # Combined M for run targets currently resolves multiple binaries and exits non-zero by design.
     run_case "run M=<all-modules-combined> (expected non-zero)" "2" \
-      "${MAKE_BASE[@]}" run "M=$all_modules_csv"
+      "${MAKE_BASE[@]}" run "M=$runtime_modules_csv"
     run_case "run-valgrind M=<all-modules-combined> (expected non-zero)" "2" \
-      "${MAKE_BASE[@]}" run-valgrind "M=$all_modules_csv" "VALGRIND_RUN_SECONDS=$VALGRIND_RUN_SECONDS"
+      "${MAKE_BASE[@]}" run-valgrind "M=$runtime_modules_csv" "VALGRIND_RUN_SECONDS=$VALGRIND_RUN_SECONDS"
   else
     skip_case "run target matrix" "timeout utility not found (need timeout or gtimeout)"
   fi
@@ -163,7 +183,7 @@ run_case "tidy" "0" "${MAKE_BASE[@]}" tidy
 
 if [ "$INCLUDE_HEAVY" = "1" ]; then
   run_case "warnings" "0" "${MAKE_BASE[@]}" warnings
-  run_case "all" "0" "${MAKE_BASE[@]}" all
+  run_case "all" "$all_expected_rc" "${MAKE_BASE[@]}" all
 else
   skip_case "warnings" "INCLUDE_HEAVY=0"
   skip_case "all" "INCLUDE_HEAVY=0"
