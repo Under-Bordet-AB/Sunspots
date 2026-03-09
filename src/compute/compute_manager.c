25a1e00d893641d80cead89b8eeeabe9363e18be
Gustav Frisén <gustav.frisen@outlook.com>
2026-03-09 10:07:49 +0100

    Fix elpris slots not loading if less than 96 found

--- src/compute/compute_manager.c
+++ src/compute/compute_manager.c
@@ -391,7 +391,7 @@ int load_data(compute_data_t* out) {
 
     for (int m = 0; m < 4; m++) {
         ss_sdk_samples_out samples = {0};
-        ss_sdk_status status = ss_sdk_db_get_canonical(0, horizon_slots, metrics[m], &samples);
+        ss_sdk_status status = ss_sdk_db_get_canonical(0, 0, metrics[m], &samples);
 
         if (status != SS_SDK_OK && status != SS_SDK_ERR_PARTIAL_DATA) {
             syslog(LOG_ERR, "Compute Manager - ss_sdk_db_get_canonical failed for metric=%d status=%d", (int)metrics[m], (int)status);
