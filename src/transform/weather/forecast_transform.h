635112aeb4b3314fd06397e4bfee94e58ef0f335
Patrik Johansson <Patrik.Viby@gmail.com>
2026-03-23 10:01:21 +0100

    Doxygen for /transform

--- src/transform/weather/forecast_transform.h
+++ src/transform/weather/forecast_transform.h
@@ -5,6 +5,26 @@
 #include "forecast_model.h"
 #include "cJSON.h"
 
+/**
+ * @brief Convert Open-Meteo hourly forecast JSON to internal model.
+ *
+ * Parses the "hourly" section of the input JSON and fills @p out with
+ * an array of weather_data_t.
+ *
+ * @param input JSON object containing forecast data.
+ * @param out Output structure to populate (should be initialized).
+ *
+ * @return transform_status_t Status code indicating success or failure.
+ *
+ * @retval TRANSFORM_OK Success.
+ * @retval TRANSFORM_INVALID_INPUT Input is NULL or not an object.
+ * @retval TRANSFORM_MISSING_FIELD Required fields are missing.
+ * @retval TRANSFORM_BAD_FORMAT Invalid field types or array mismatch.
+ * @retval TRANSFORM_UNSUPPORTED Unsupported units.
+ *
+ * @note Memory for @p out->weather_arr is allocated internally and must
+ *       be freed with forecast_data_dispose().
+ */
 transform_status_t transform_openmeteo_forecast(const cJSON *input, forecast_data_t *out);
 
 #endif
--- src/transform/weather/weather_model.h
+++ src/transform/weather/weather_model.h
@@ -4,6 +4,13 @@
 #include <time.h>
 #include <stdbool.h>
 
+
+/**
+ * @brief Represents a single weather data point.
+ *
+ * Contains timestamped weather measurements. Each value has a corresponding
+ * flag indicating whether it is present.
+ */
 typedef struct {
     time_t timestamp_unix;
     double temperature_c;
@@ -14,6 +21,14 @@ typedef struct {
     bool has_solar_radiation;
 } weather_data_t;
 
+
+/**
+ * @brief Initialize a weather_data_t structure.
+ *
+ * Sets all values to defaults and marks all fields as not present.
+ *
+ * @param data Pointer to the structure to initialize.
+ */
 void weather_data_init(weather_data_t *data);
 
 #endif
