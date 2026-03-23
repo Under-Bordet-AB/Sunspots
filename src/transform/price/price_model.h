f10b895006d69bae519debf92a5df85f2e2c0a88
Patrik Johansson <Patrik.Viby@gmail.com>
2026-03-23 10:06:38 +0100

    More Doxygen

--- src/transform/price/price_model.h
+++ src/transform/price/price_model.h
@@ -4,6 +4,12 @@
 #include <time.h>
 #include <stdbool.h>
 
+/**
+ * @brief Represents electricity price data.
+ *
+ * Stores a time series of prices in SEK per kWh along with timestamps
+ * and a region identifier.
+ */
 typedef struct {
     time_t *timestamp_arr_unix;
     double *price_arr_SEK_per_kWh;
@@ -11,7 +17,25 @@ typedef struct {
     int region;
 } price_data_t;
 
+
+/**
+ * @brief Initialize a price_data_t structure.
+ *
+ * Sets pointers to NULL and initializes metadata.
+ *
+ * @param data Pointer to the structure to initialize.
+ * @param region Region identifier to assign.
+ */
 void price_data_init(price_data_t *data, int region);
+
+
+/**
+ * @brief Free resources used by a price_data_t structure.
+ *
+ * Frees allocated arrays and resets the structure.
+ *
+ * @param data Pointer to the structure to dispose.
+ */
 void price_data_dispose(price_data_t *data);
 
 #endif
